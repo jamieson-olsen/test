15366f5d458f9d1c8c41fa7cbe0e87d34134ba16
Jamieson Olsen <jamieson@fnal.gov>
2020-05-14 09:49:02 -0500

    got rid of dev and develop branches merged back into master

--- hello.c
+++ hello.c
@@ -3,15 +3,12 @@
 // jamieson olsen
 // another new comment added!
 // some more comments added!
-// doing some more work in branch dev...
-
 
 #include <stdio.h>
     
 int main() 
 {
   printf("Hello, World!");
-  printf("This is a new test feature!");
   return 0;
 }
 	
